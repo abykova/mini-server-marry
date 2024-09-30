3d3bc5c361c7f8e7d975e34345630ebc602aade7
Alexandra <alex.94.girl@gmail.com>
2024-09-30 14:15:27 +0500

    Update server.py

--- server/server.py
+++ server/server.py
@@ -8,130 +8,115 @@ from server.auth import AuthManager
 
 class MyHandler(SimpleHTTPRequestHandler):
     auth_manager = AuthManager()
-    cipher = AESCipher(os.urandom(32))
     
     def __init__(self, *args, **kwargs):
+        # Инициализация шифровальщика при создании сервера
         self.cipher = AESCipher(os.urandom(32))
         super().__init__(*args, **kwargs)
 
     def do_GET(self):
+        # Обработка главной страницы
         if self.path == '/':
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
             with open("static/index.html", "rb") as file:
                 self.wfile.write(file.read())
-        elif self.path.startswith('/encrypt?'):
-            query = urllib.parse.urlparse(self.path).query
-            params = urllib.parse.parse_qs(query)
-            message = params.get('message', [None])[0]
-            if message:
-                encrypted_message = self.cipher.encrypt(message)
-                self.send_response(200)
-                self.send_header("Content-type", "text/plain")
-                self.end_headers()
-                self.wfile.write(encrypted_message.hex().encode())
-            else:
-                self.send_error(400, "Bad Request: message parameter is missing")
-        elif self.path.startswith('/decrypt?'):
-            query = urllib.parse.urlparse(self.path).query
-            params = urllib.parse.parse_qs(query)
-            ciphertext = params.get('ciphertext', [None])[0]
-            if ciphertext:
-                try:
-                    decrypted_message = self.cipher.decrypt(bytes.fromhex(ciphertext))
-                    self.send_response(200)
-                    self.send_header("Content-type", "text/plain")
-                    self.end_headers()
-                    self.wfile.write(decrypted_message.encode())
-                except Exception as e:
-                    self.send_error(400, f"Bad Request: {str(e)}")
-            else:
-                self.send_error(400, "Bad Request: ciphertext parameter is missing")
 
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            with open("static/index.html", "rb") as file:
-                self.wfile.write(file.read())
+        # Обработка маршрута для шифрования
+        elif self.path.startswith('/encrypt'):
+            self.handle_encrypt()
+
+        # Обработка маршрута для дешифрования
+        elif self.path.startswith('/decrypt'):
+            self.handle_decrypt()
+
+        # Обработка регистрации пользователей
         elif self.path.startswith('/register'):
-            query_components = parse_qs(urlparse(self.path).query)
-            username = query_components.get('username', [None])[0]
-            password = query_components.get('password', [None])[0]
+            self.handle_register()
 
-            if username and password:
-                success, message = self.auth_manager.register(username, password)
-                self.send_response(200 if success else 400)
-                self.send_header("Content-type", "text/plain")
-                self.end_headers()
-                self.wfile.write(message.encode())
-            else:
-                self.send_error(400, "Username and password required")
+        # Обработка входа пользователей
         elif self.path.startswith('/login'):
-            query_components = parse_qs(urlparse(self.path).query)
-            username = query_components.get('username', [None])[0]
-            password = query_components.get('password', [None])[0]
+            self.handle_login()
 
-            if username and password:
-                success, message = self.auth_manager.login(username, password)
-                self.send_response(200 if success else 400)
+        else:
+            self.send_error(404, "Endpoint not found")
+
+    def handle_encrypt(self):
+        """Метод для обработки запросов на шифрование текста"""
+        query = urllib.parse.urlparse(self.path).query
+        params = urllib.parse.parse_qs(query)
+        message = params.get('message', [None])[0]
+
+        if message:
+            encrypted_message = self.cipher.encrypt(message)
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(encrypted_message.hex().encode())
+        else:
+            self.send_error(400, "Bad Request: message parameter is missing")
+
+    def handle_decrypt(self):
+        """Метод для обработки запросов на дешифрование текста"""
+        query = urllib.parse.urlparse(self.path).query
+        params = urllib.parse.parse_qs(query)
+        ciphertext = params.get('ciphertext', [None])[0]
+
+        if ciphertext:
+            try:
+                decrypted_message = self.cipher.decrypt(bytes.fromhex(ciphertext))
+                self.send_response(200)
                 self.send_header("Content-type", "text/plain")
                 self.end_headers()
-                self.wfile.write(message.encode())
-            else:
-                self.send_error(400, "Username and password required")
+                self.wfile.write(decrypted_message.encode())
+            except Exception as e:
+                self.send_error(400, f"Decryption failed: {str(e)}")
+        else:
+            self.send_error(400, "Bad Request: ciphertext parameter is missing")
 
-    def do_GET(self):
-            if self.path == '/':
-                self.send_response(200)
-                self.send_header("Content-type", "text/html")
-                self.end_headers()
-                with open("static/index.html", "rb") as file:
-                    self.wfile.write(file.read())
-            elif self.path.startswith('/encrypt'):
-                query = urllib.parse.urlparse(self.path).query
-                params = urllib.parse.parse_qs(query)
-                message = params.get('message', [None])[0]
-    
-                if message:
-                    encrypted_message = self.cipher.encrypt(message)
-                    self.send_response(200)
-                    self.send_header("Content-type", "text/plain")
-                    self.end_headers()
-                    self.wfile.write(encrypted_message.hex().encode())
-                else:
-                    self.send_error(400, "Message parameter is missing")
-    
-            elif self.path.startswith('/decrypt'):
-                query = urllib.parse.urlparse(self.path).query
-                params = urllib.parse.parse_qs(query)
-                ciphertext = params.get('ciphertext', [None])[0]
-    
-                if ciphertext:
-                    try:
-                        decrypted_message = self.cipher.decrypt(bytes.fromhex(ciphertext))
-                        self.send_response(200)
-                        self.send_header("Content-type", "text/plain")
-                        self.end_headers()
-                        self.wfile.write(decrypted_message.encode())
-                    except Exception as e:
-                        self.send_error(400, f"Decryption failed: {str(e)}")
-                else:
-                    self.send_error(400, "Ciphertext parameter is missing")
+    def handle_register(self):
+        """Метод для регистрации пользователей"""
+        query_components = parse_qs(urlparse(self.path).query)
+        username = query_components.get('username', [None])[0]
+        password = query_components.get('password', [None])[0]
+
+        if username and password:
+            success, message = self.auth_manager.register(username, password)
+            self.send_response(200 if success else 400)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(message.encode())
+        else:
+            self.send_error(400, "Username and password required")
+
+    def handle_login(self):
+        """Метод для обработки логина пользователей"""
+        query_components = parse_qs(urlparse(self.path).query)
+        username = query_components.get('username', [None])[0]
+        password = query_components.get('password', [None])[0]
+
+        if username and password:
+            success, message = self.auth_manager.login(username, password)
+            self.send_response(200 if success else 400)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(message.encode())
+        else:
+            self.send_error(400, "Username and password required")
 
 def run_https(server_class=HTTPServer, handler_class=MyHandler, port=8443):
+    """Функция для запуска HTTPS-сервера"""
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
 
-    # SSL
+    # Настройка SSL для безопасного соединения
     httpd.socket = ssl.wrap_socket(httpd.socket,
                                    keyfile="key.pem",
                                    certfile="cert.pem",
                                    server_side=True)
 
-    print(f"Starting https server on port {port}")
+    print(f"Starting HTTPS server on port {port}")
     httpd.serve_forever()
 
 if __name__ == "__main__":
